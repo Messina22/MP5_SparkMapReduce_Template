1b0ec0d4fcfbdb788afa2e31bccdd605b07ceace
Nick Messina <nicknmessina@gmail.com>
2024-03-02 09:19:22 -0600

    Finished partE

--- PythonTemplate/PopularityLeagueSpark.py
+++ PythonTemplate/PopularityLeagueSpark.py
@@ -10,16 +10,24 @@ sc = SparkContext(conf=conf)
 
 lines = sc.textFile(sys.argv[1], 1) 
 
-#TODO
+def parse_links(line):
+    parts = line.split(':')
+    linked_ids = parts[1].split()
+    return [(linked_id, 1) for linked_id in linked_ids]
+
+link_counts = lines.flatMap(parse_links).reduceByKey(lambda a, b: a + b)
 
 leagueIds = sc.textFile(sys.argv[2], 1)
+ids = leagueIds.collect()
+league_counts = link_counts.filter(lambda x: x[0] in ids)
+counts = league_counts.values().collect()
 
-#TODO
+ranked_links = league_counts.map(lambda x: (x[0], sum([1 for count in counts if count < x[1]]))).sortByKey()
 
 output = open(sys.argv[3], "w")
 
-#TODO
-#write results to output file. Foramt for each line: (key + \t + value +"\n")
+for (link, rank) in ranked_links.collect():
+    output.write(f'{link}\t{rank}\n')
 
 sc.stop()
 
