00c21581fae393869aad0fcda72be3a231de1ac0
Nick Messina <nicknmessina@gmail.com>
2024-03-01 21:21:19 -0600

    Parts A & B implemented

--- PythonTemplate/TitleCountSpark.py
+++ PythonTemplate/TitleCountSpark.py
@@ -9,10 +9,10 @@ stopWordsPath = sys.argv[1]
 delimitersPath = sys.argv[2]
 
 with open(stopWordsPath) as f:
-	#TODO
+    stop_words = set(f.read().splitlines())
 
 with open(delimitersPath) as f:
-    #TODO
+    delimiters = f.read().strip()
 
 conf = SparkConf().setMaster("local").setAppName("TitleCount")
 conf.set("spark.driver.bindAddress", "127.0.0.1")
@@ -20,11 +20,25 @@ sc = SparkContext(conf=conf)
 
 lines = sc.textFile(sys.argv[3], 1)
 
-#TODO
+def tokenize(title, delimiters):
+    title = title.strip().lower()
+    
+    for delimiter in delimiters:
+        title = title.replace(delimiter, ' ')
+    
+    tokens = title.split()
+    return [token for token in tokens if token]
 
+tokens = lines.flatMap(lambda line: tokenize(line, delimiters)).filter(lambda token: token not in stop_words)
+
+wordCounts = tokens.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+
+topWords = wordCounts.takeOrdered(10, key=lambda x: -x[1])
+topWords = sorted(topWords, key=lambda x: x[0])
+            
 outputFile = open(sys.argv[4],"w")
 
-#TODO
-#write results to output file. Foramt for each line: (line +"\n")
+for (word, count) in topWords:
+    outputFile.write(f"{word}\t{count}\n")
 
 sc.stop()
--- PythonTemplate/TopTitleStatisticsSpark.py
+++ PythonTemplate/TopTitleStatisticsSpark.py
@@ -7,19 +7,21 @@ conf.set("spark.driver.bindAddress", "127.0.0.1")
 sc = SparkContext(conf=conf)
 
 lines = sc.textFile(sys.argv[1], 1)
-
-#TODO
+wordCounts = lines.map(lambda line: line.split('\t'))
+counts = wordCounts.map(lambda x: int(x[1]))
+mean = counts.mean()
+sum = counts.sum()
+min = counts.min()
+max = counts.max()
+var = counts.variance()
 
 outputFile = open(sys.argv[2], "w")
-'''
-TODO write your output here
-write results to output file. Format
-outputFile.write('Mean\t%s\n' % ans1)
-outputFile.write('Sum\t%s\n' % ans2)
-outputFile.write('Min\t%s\n' % ans3)
-outputFile.write('Max\t%s\n' % ans4)
-outputFile.write('Var\t%s\n' % ans5)
-'''
+
+outputFile.write('Mean\t%s\n' % int(mean))
+outputFile.write('Sum\t%s\n' % sum)
+outputFile.write('Min\t%s\n' % min)
+outputFile.write('Max\t%s\n' % max)
+outputFile.write('Var\t%s\n' % int(var))
 
 sc.stop()
 
