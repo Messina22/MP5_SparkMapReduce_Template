d41531791c0f2917fd81be163af57583ec792fbc
Nick Messina <nicknmessina@gmail.com>
2024-03-02 00:38:43 -0600

    Finished partD

--- PythonTemplate/TopPopularLinksSpark.py
+++ PythonTemplate/TopPopularLinksSpark.py
@@ -8,12 +8,28 @@ sc = SparkContext(conf=conf)
 
 lines = sc.textFile(sys.argv[1], 1) 
 
-#TODO
+def parse_links(line):
+    parts = line.split(':')
+    linked_ids = parts[1].split()
+    links = []
+    for linked_id in linked_ids:
+        links.append((linked_id, 1))
+    return links
+
+# Flatten the list of links
+links = lines.flatMap(parse_links)
+
+# Reduce by key to sum the counts
+reduced_counts = links.reduceByKey(lambda a, b: a + b)
+
+topLinks = reduced_counts.takeOrdered(10, key=lambda x: -x[1])
+topLinks = sorted(topLinks, key=lambda x: x[0])
 
 output = open(sys.argv[2], "w")
-
-#TODO
-#write results to output file. Foramt for each line: (key + \t + value +"\n")
+ 
+#write results to output file. Foramt for each line: (line + "\n")
+for (link, count) in topLinks:
+    output.write(f'{link}\t{count}\n')
 
 sc.stop()
 
